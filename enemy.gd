03353f0eb1aea23cde5112165dc98e76fa35b6d8
Russell Matney <russell.matney@gmail.com>
2021-06-19 00:12:59 -0400

    feats: enemy colors, player gun, 10 bullet cap
    Some other fixes, like messing with bullet physics a bit, adding a
    bullet script, map re-drawing.

--- enemy.gd
+++ enemy.gd
@@ -4,24 +4,57 @@ extends KinematicBody2D
 var motion = Vector2()
 var player
 
+var dying = false
+var dying_timer = 2
+
+onready var sprite = $Sprite
+var colors = [
+  Color("be4444"), # red
+  Color("be8744"), # orange
+  Color("2f8918"), # green
+  Color("811889"), # purple
+  Color("891846"), # magenta
+  ]
+var health = colors.size()
+var index = 0
+
 
 func _ready():
   player = get_parent().get_node("player")
+  index = randi() % colors.size()
+  sprite.modulate = colors[index]
 
 
 func _physics_process(delta):
+  if dying:
+    set_scale(Vector2(scale.x/1.03, scale.y/1.04))
+  else:
+    move(delta)
+
+func move(delta):
   position += (player.position - position) / 50
 
   look_at(player.position)
 
   move_and_collide(motion)
 
+func hit():
+  health -= 1
+  if health < 1:
+    kill()
+    return
+
+  index = (index + 1) % colors.size()
+  sprite.modulate = colors[index]
 
 func kill():
+  dying = true
+  yield(get_tree().create_timer(dying_timer), "timeout")
+  dying = false
   queue_free()
 
 
 func _on_Area2D_body_entered(body):
-  # TODO walk the rainbow
   if "bullet" in body.name:
-    kill()
+    hit()
+    body.kill()
--- player.gd
+++ player.gd
@@ -3,9 +3,17 @@ extends KinematicBody2D
 var speed = 500
 var velocity = Vector2()
 
+onready var gun = $gun
 var bullet_speed = 2000
 var bullet = preload("res://bullet.tscn")
 
+var bullets = []
+var to_remove
+var current_bullet = 0
+var max_bullets = 10
+
+var fire_debounce = 0.2
+var fire_cooldown = false
 
 func _physics_process(_delta):
   var v_diff = Vector2()
@@ -25,15 +33,33 @@ func _physics_process(_delta):
   look_at(get_global_mouse_position())
 
   if Input.is_action_pressed("ui_action"):
-    fire()
+    if not fire_cooldown:
+      fire()
 
 
 func fire():
   var new_bullet = bullet.instance()
-  new_bullet.position = get_global_position()
+  new_bullet.position = gun.get_global_position()
   new_bullet.rotation_degrees = rotation_degrees
   new_bullet.apply_impulse(Vector2(), Vector2(bullet_speed, 0).rotated(rotation))
   get_tree().get_root().call_deferred("add_child", new_bullet)
+
+  if bullets.size() - 1 > current_bullet:
+    to_remove = bullets[current_bullet]
+    if to_remove and is_instance_valid(to_remove):
+      to_remove.kill()
+    bullets[current_bullet] = new_bullet
+  else:
+    bullets.append(new_bullet)
+
+  # increment bullet index
+  current_bullet += 1
+  if current_bullet > max_bullets:
+    current_bullet = 0
+
+  fire_cooldown = true
+  yield(get_tree().create_timer(fire_debounce), "timeout")
+  fire_cooldown = false
 
 
 func kill():
